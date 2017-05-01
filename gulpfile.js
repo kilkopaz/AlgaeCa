a28ec74bf16fd10a2ee14834c8228eae62b0a64c
Kilko Paz <kilkopaz@unm.edu>
2017-04-30 21:48:45 -0600

    Installed Browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('hello', function() {
 	console.log("Hello Kil");
 });
 
-gulp.task('sass', function(){
-  return gulp.src('app/scss/styles.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  // Other watchers
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
