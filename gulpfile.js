340b74b15161643d6aec5956351e9a5424ed090a
Kilko Paz <kilkopaz@unm.edu>
2017-05-02 13:38:08 -0600

    Add Git .gitignore file. add sass structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
+//Including plugins
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
-
-gulp.task('hello', function() {
-	console.log("Hello Kil");
-});
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
@@ -15,11 +12,6 @@ gulp.task('sass', function() {
     }))
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function (){
-  gulp.watch('app/scss/**/*.scss', ['sass']); 
-  // Other watchers
-});
-
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
@@ -27,3 +19,11 @@ gulp.task('browserSync', function() {
     },
   })
 })
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  // Other watchers
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
+
